f18b762b590751efbc2c1d7bd6c3e9f5d64f6cb9
Dakota States <dakota.states@gmail.com>
2020-11-19 12:13:44 -0800

    Auth, application, and user controllers, user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :occupation, :description, :image, :bio, :contactmessage, :email, :phone, :website, :resumedownload
 end
